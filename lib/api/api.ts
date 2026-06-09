f9a7150d69bf6ae11e9fa749b14b2f20c29f05cb
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-06-09 23:46:42 +0300

    Update

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,12 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
-if (!baseURL) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined");
-}
-
 export const nextServer = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
   withCredentials: true,
 });
